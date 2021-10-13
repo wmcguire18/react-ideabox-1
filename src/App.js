cb23bd6fa0074714f4a98a98abab2ef1833d5179
William McGuire <wmcguire18@gmail.com>
2021-10-13 13:03:25 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Ideas from './Ideas';
+import Form from './Form';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ideas: [
+        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+       { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+       { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+      ]
+    }
+  }
+
+addIdea = (newIdea) => {
+  this.setState({ideas: [...this.state.ideas, newIdea] });
+}
+
+deleteIdea = (id) => {
+  const filteredIdeas = this.state.ideas.filter(idea => idea.id != id);
+
+  this.setState({ideas: filteredIdeas});
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <main className ='App'>
+    <h1>IdeaBox</h1>
+    {!this.state.ideas.length && <h2>No ideas yet -- add some!</h2> }
+    <Form addIdea={this.addIdea} />
+    <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea}/>
+    </main>
+    )
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Card.css';
+
+const Card = ({ title, description, id, deleteIdea }) => {
+  return (
+    <div className='card'>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <button onClick={() => deleteIdea(id)}>🗑</button>
+    </div>
+  )
+}
+
+export default Card;
